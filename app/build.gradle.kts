bcb68849f62bd943792431a3a9e04d9e73676548
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-10-27 15:49:55 +0100

    Migrate from Groovy to Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id(BuildPlugins.androidApplication)
+    id(BuildPlugins.kotlinAndroid)
+    id(BuildPlugins.kotlinAndroidExtensions)
+}
+
+android {
+    compileSdkVersion(AndroidSdk.compile)
+
+    defaultConfig {
+        applicationId = "com.luca020400.classiperlo"
+        minSdkVersion(AndroidSdk.min)
+        targetSdkVersion(AndroidSdk.target)
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    // Kotlin
+    implementation(Libraries.kotlinStdLib)
+
+    // AndroidX
+    implementation(Libraries.appCompat)
+    implementation(Libraries.constraintLayout)
+    implementation(Libraries.ktxCore)
+    implementation(Libraries.lifecycle)
+    implementation(Libraries.vectordrawable)
+
+    // Material
+    implementation(Libraries.material)
+
+    // Navigation
+    implementation(Libraries.navigationFragment)
+    implementation(Libraries.navigationFragmentKtx)
+    implementation(Libraries.navigationUi)
+    implementation(Libraries.navigationUiKtx)
+}
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,48 @@
+object Kotlin {
+    const val kotlinVersion = "1.3.50"
+}
+
+object BuildPlugins {
+    private object Versions {
+        const val buildToolsVersion = "3.5.1"
+    }
+
+    const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.buildToolsVersion}"
+    const val kotlinGradlePlugin =
+        "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.kotlinVersion}"
+    const val androidApplication = "com.android.application"
+    const val kotlinAndroid = "kotlin-android"
+    const val kotlinAndroidExtensions = "kotlin-android-extensions"
+}
+
+object AndroidSdk {
+    const val min = 15
+    const val compile = 29
+    const val target = compile
+}
+
+object Libraries {
+    private object Versions {
+        const val appcompat = "1.1.0"
+        const val constraintLayout = "1.1.3"
+        const val ktx = "1.1.0"
+        const val lifecycle = "2.1.0"
+        const val vectordrawable = "1.1.0"
+        const val material = "1.0.0"
+        const val navigation = "2.1.0"
+    }
+
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Kotlin.kotlinVersion}"
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val constraintLayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    const val ktxCore = "androidx.core:core-ktx:${Versions.ktx}"
+    const val lifecycle = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
+    const val vectordrawable = "androidx.vectordrawable:vectordrawable:${Versions.vectordrawable}"
+    const val material = "com.google.android.material:material:${Versions.material}"
+    const val navigationFragment = "androidx.navigation:navigation-fragment:${Versions.navigation}"
+    const val navigationFragmentKtx =
+        "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+    const val navigationUi = "androidx.navigation:navigation-ui:${Versions.navigation}"
+    const val navigationUiKtx = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+}
