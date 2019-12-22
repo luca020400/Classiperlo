f750123330ab0263416f6843e44d6efc89b28147
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-12-22 13:55:25 +0100

    Use okhttp to make all the requests

--- app/src/main/java/com/luca020400/classiperlo/ui/cp/CpViewModel.kt
+++ app/src/main/java/com/luca020400/classiperlo/ui/cp/CpViewModel.kt
@@ -5,13 +5,26 @@ import androidx.lifecycle.liveData
 import com.luca020400.classiperlo.classes.DataItem
 import com.luca020400.classiperlo.classes.Url
 import kotlinx.coroutines.Dispatchers
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.jsoup.Jsoup
 
 class CpViewModel : ViewModel() {
+    private val client = OkHttpClient()
+
     val data = liveData(Dispatchers.IO) {
-        val doc = Jsoup.connect("http://www.classiperlo.altervista.org/index.html").get()
-        emit(doc.select("#cssmenu > ul > li").drop(1).map {
-            DataItem(it.text(), Url(it.select("a").attr("href")))
-        }.toMutableList())
+        val request = Request.Builder().url("http://www.classiperlo.altervista.org/index.html")
+            .get().build()
+        try {
+            client.newCall(request).execute().use {
+                if (!it.isSuccessful) throw Exception("Unexpected code $it")
+
+                val doc = Jsoup.parse(it.body?.string())
+                emit(doc.select("#cssmenu > ul > li").drop(1).map { element ->
+                    DataItem(element.text(), Url(element.select("a").attr("href")))
+                }.toMutableList())
+            }
+        } catch (e: Exception) {
+        }
     }
 }
